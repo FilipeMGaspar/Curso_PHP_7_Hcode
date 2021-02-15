d117c2d5307e3d41eed65a3f6c4c7418b40f9304
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 17:50:46 +0000

    Aula_14 -> Conhecendo os Tipos de Dados em PHP

--- Secao_3/Aula_14.php
+++ Secao_3/Aula_14.php
@@ -1,4 +1,10 @@
 <?php
 	$anoNascimento = 1990;
 	$nomeCompleto= "Antonio dos Santos";
+
+	$nome1= "João";
+
+	//$1nome = "João"; //Declaração incorrecta não deve inicair as variaveis com números
+	//$%nome = "João"; //Declaração incorrecta Não utilize caracteres especiais
+	
 ?>
