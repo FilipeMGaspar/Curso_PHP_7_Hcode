d1a19145e0eaaa2fa8156043283d2b058f7b4b02
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 18:12:28 +0000

    Aula_14 -> Conhecendo os Tipos de Dados em PHP

--- Secao_3/Aula_14_02.php
+++ Secao_3/Aula_14_02.php
@@ -1,3 +1,15 @@
 <?php
+	//Tipos de Dados em PHP
 
+	//tipos básicos de dados
+	$nome = "Hcode";//string
+	$site = 'www.hcode.com.br';//string
+
+	$ano = 1990;//int
+
+	$salario = 550.55;//double, float
+
+	$bloqueado = false;//boleano
+
+	//
 ?>
