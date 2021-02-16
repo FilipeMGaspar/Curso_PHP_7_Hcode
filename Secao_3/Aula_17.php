9e23d71826e530b8d2db6edd175675635bbb2717
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 15:43:35 +0000

    Aula_17 -> Conhecendo os Operadores do PHP

--- Secao_3/Aula_17.php
+++ Secao_3/Aula_17.php
@@ -2,30 +2,28 @@
 	//Operadores de atribuição
 	$nome = "Hcode";
 	echo "<h2>Operadores de atribuição</h2>";
-	echo "<br><br>";
 	echo $nome . " mais alguma coisa";
 
 	$nome .= " Treinamentos";
 
-	echo "<br><br>";
+	echo "<br>";
 	echo $nome;
 
-	echo "<br><br>";
+	echo "<br>";
 	$valorTotal = 0;
 
 	$valorTotal += 100;
 	$valorTotal += 25;
 
 	echo $valorTotal;
-	echo "<br><br>";
+	echo "<br>";
 	$valorTotal -= 10;
 	echo $valorTotal;
-	echo "<br><br>";
+	echo "<br>";
 	$valorTotal *= 0.9;
 	echo $valorTotal;
 
 	//Operadores Aritméticos
-	echo "<br><br>";
 	echo "<h2>Operadores Aritméticos</h2>";
 
 	$a = 10;
@@ -33,6 +31,10 @@
 
 	echo "Soma = " . ($a + $b);
 
-	echo "<br><br>Subtração = " . ($a - $b);
+	echo "<br>Subtração = " . ($a - $b);
 
+	echo "<br> Multiplicação =" . ($a * $b);
+
+	echo "<br>Divisão = " . ($a / $b);
+	
 ?>
