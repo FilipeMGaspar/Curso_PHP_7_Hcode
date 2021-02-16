920b1c9331663fa52416b5be7a64643278e3b96b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 15:14:57 +0000

    Aula_16 -> Escopo de Variável

--- Secao_3/Aula_16.php
+++ Secao_3/Aula_16.php
@@ -1,8 +1,16 @@
 <?php
+	//escopo global
 	$nome = "Glaucio";
 
 	function teste() {
 		echo $nome;
 	}
-	
+
+	function teste2() {
+		$Sobrenome = "Qualquer";
+	}
+
+	teste();
+
+	echo "$Sobrenome";
 ?>
