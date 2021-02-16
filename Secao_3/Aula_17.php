9e98299a3062edd33e60184c6549a939bbe78fc0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 15:36:48 +0000

    Aula_17 -> Conhecendo os Operadores do PHP

--- Secao_3/Aula_17.php
+++ Secao_3/Aula_17.php
@@ -1,7 +1,8 @@
 <?php
 	//Operadores de atribuição
 	$nome = "Hcode";
-
+	echo "<h2>Operadores de atribuição</h2>";
+	echo "<br><br>";
 	echo $nome . " mais alguma coisa";
 
 	$nome .= " Treinamentos";
@@ -9,4 +10,21 @@
 	echo "<br><br>";
 	echo $nome;
 
+	echo "<br><br>";
+	$valorTotal = 0;
+
+	$valorTotal += 100;
+	$valorTotal += 25;
+
+	echo $valorTotal;
+	echo "<br><br>";
+	$valorTotal -= 10;
+	echo $valorTotal;
+	echo "<br><br>";
+	$valorTotal *= 0.9;
+	echo $valorTotal;
+
+	//Operadores Aritméticos
+	echo "<br><br>";
+	echo "<h2>Operadores Aritméticos</h2>";
 ?>
