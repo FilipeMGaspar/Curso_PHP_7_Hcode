c0efd4fd604211f92c250719d93ae30d2173152f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 13:07:25 +0000

    Aula_15 -> Variáveis Pré-definidas

--- Secao_3/Aula_15.php
+++ Secao_3/Aula_15.php
@@ -1,7 +1,12 @@
 <?php
 
-	$nome=(int)$_GET["a"];
-	var_dump($nome);
+	$a = (int)$_GET["a"];
+	$b = (int)$_GET["b"];
+	//var_dump($nome);
+
+	echo $a;
+	echo "<br>";
+	echo $b;
 	echo "<br><br>";
 
 
