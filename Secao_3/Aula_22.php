ebf1e6eae65a34b548e7289bb45f41f329feedc8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-18 15:10:54 +0000

    Aula_22 -> For

--- Secao_3/Aula_22.php
+++ Secao_3/Aula_22.php
@@ -4,8 +4,14 @@ for ($i = 0; $i<=100; $i+=5) {
 	if ($i > 55 && $i < 75) continue;
 
 	if ($i === 95) break;
-	
+
 	echo $i." - ";
 }
-echo " Fim!";
+echo " Fim!<br>";
+
+
+//Não fazer ou derruba o servidor
+for ($i=0; $i < 10 ; $i--) { 
+	echo $i;
+}
 ?>
