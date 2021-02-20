53db43f272314a5067491474dd5e9a2310003c58
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-20 13:26:54 +0000

    Aula_23 -> Foreach

--- Secao_3/Aula_23_02.php
+++ Secao_3/Aula_23_02.php
@@ -1,5 +1,18 @@
 <form>
 	<input type="texto" name="nome">
 	<input type="date" name="nascimento">
-	<input type="submit" name="nome">
+	<input type="submit" value="OK">
 </form>>
+
+<?php
+	
+	if(isset($_GET)){
+		foreach ($_GET as $key => $value) {
+			echo "Nome do Campo ". $key;
+
+			echo "Valor do campo ". $value;
+
+			echo "<hr>";
+		}
+	}
+?>
