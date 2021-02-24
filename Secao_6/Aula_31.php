6edd27517792b8ced68b6a4513aa0d3ba49ed3d7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-24 20:33:17 +0000

    Aula_31 ->Funções Para Sessão

--- Secao_6/Aula_31.php
+++ Secao_6/Aula_31.php
@@ -3,5 +3,16 @@
 
 	echo session_save_path();
 	echo "<br>";
-	var_dump(session_start());
+	var_dump(session_status());
+
+	echo "<br><br>";
+	switch (session_status()) {
+		case PHP_SESSION_DISABLE:
+			echo "string";
+			break;
+		
+		default:
+			# code...
+			break;
+	}
 ?>
