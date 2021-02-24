b33a766ec7efcc864fb6e357e64a0b6fa35064bd
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-24 16:52:01 +0000

    Aula_30 -> ID de Sessão

--- Secao_6/Aula_30.php
+++ Secao_6/Aula_30.php
@@ -1,6 +1,15 @@
 <?php
+	session_id('ln6rl1l9kl5koqerg8vjd4gk4r');
+	
 	session_start();
 
 	echo session_id();
 
+	echo "<br>";
+
+	session_regenerate_id();
+	echo "<br>".session_id();
+
+	echo "<br>";
+	var_dump($_SESSION);
 ?>
