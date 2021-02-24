222d41c87c305a98c5d83dc7ace3b73fa21e4641
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-24 16:36:18 +0000

    Aula_29 -> Criando e Entendendo Sessões

--- Secao_6/Aula_29.php
+++ Secao_6/Aula_29.php
@@ -1,5 +1,5 @@
 <?php
-	require_once Aula_29_03.php;
+	require_once "Aula_29_03.php";
 	//session_start();
 	$_SESSION['nome'] = "Hcode";
 ?>
--- Secao_6/Aula_29_02.php
+++ Secao_6/Aula_29_02.php
@@ -1,5 +1,5 @@
 <?php
-	require_once Aula_29_03.php;
+	require_once "Aula_29_03.php";
 	//session_start();
 	echo $_SESSION['nome'];
 ?>
