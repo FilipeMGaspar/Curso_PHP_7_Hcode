2008349468de1e93686b659b7c6c75c3f062b718
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-25 17:55:42 +0000

    Aula_32 -> Criando Funções de Usuário

--- Secao_7/Aula_32.php
+++ Secao_7/Aula_32.php
@@ -4,8 +4,14 @@
 		return "Olá Mundo!!!<br>";
 	}
 
+	function salario(){
+		return 756;
+	}
+
 	echo ola();
 	$frase = ola();
 
-	echo strlen($frase);
+	echo strlen($frase)."<br>";
+
+	echo "<br> José recebeu 3 salários: ".salario() * 3;
 ?>
