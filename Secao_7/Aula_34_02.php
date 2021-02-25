8db57805535f4c72aabbf6f71f5c622c188d1969
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-25 19:22:16 +0000

    Aula_34 ->Parâmetros por Valor x por Referência

--- Secao_7/Aula_34_02.php
+++ Secao_7/Aula_34_02.php
@@ -6,8 +6,13 @@
 		'idade' => 20
 	);
 
-	foreach ($pessoa as $value) {
+	//&$value passagem por referencia
+	foreach ($pessoa as &$value) {
+		if(gettype($value) === 'integer'){
+			$value += 10;
+		}
 		echo $value ."<br>";
 	}
 
+	print_r($pessoa);
 ?>
