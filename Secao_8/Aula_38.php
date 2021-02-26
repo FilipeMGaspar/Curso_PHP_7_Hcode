7349d7f9721c267b4dd52dcab2f3bb070abb577e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-26 15:35:54 +0000

    Aula_37 ->Funções Anônimas

--- Secao_8/Aula_38.php
+++ Secao_8/Aula_38.php
@@ -6,7 +6,8 @@
 	echo time();
 
 	echo "<br>";
-	$ts = strtotime("2001-09-11");
+	//$ts = strtotime("2001-09-11");
+	$ts = strtotime(now);
 
-	echo date("d/m/Y", $ts);
+	echo date("l, d/m/Y", $ts);
 ?>
