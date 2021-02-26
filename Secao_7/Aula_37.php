fc8aca3e439af4714451c8a7f4bc7c431258366d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-26 15:05:41 +0000

    Aula_37 ->Funções Anônimas

--- Secao_7/Aula_37.php
+++ Secao_7/Aula_37.php
@@ -1,12 +1,15 @@
 <?php
 	function test($callback){
 		//Processo lento
-
+		int c = 0;
+		do{
+			c++;
+		}while(c <= 100000);
 		$callback();
 	} 
 
 	test(function(){
-		echo "Terminou";
+		echo "Terminou!";
 	});
 
 ?>
