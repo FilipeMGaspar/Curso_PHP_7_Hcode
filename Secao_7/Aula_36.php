4e8a1a531407ff9b4788df525b29bbf83edda50f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-26 13:52:10 +0000

    Aula_36 ->Funções Recursivas

--- Secao_7/Aula_36.php
+++ Secao_7/Aula_36.php
@@ -1,3 +1,15 @@
 <?php
-	
+	$hiarquia = array(
+		array(
+			'nome_cargo' => 'CEO'
+			'subordinados' => array(
+				//Inicio: Diretor
+				array(
+					'nome_cargo' => 'Diretor Comercial'
+				),
+
+				array()
+			)
+		)
+	);
 ?>
