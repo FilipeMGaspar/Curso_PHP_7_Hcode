959194280c179422479964f3f051c1b3868e8681
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-26 13:38:54 +0000

    Aula_35 ->Funções no PHP 7 Novidades

--- Secao_7/Aula_35.php
+++ Secao_7/Aula_35.php
@@ -1,11 +1,12 @@
 <?php
   function soma(int ...$valores){
-  	return array(&valores);
+  	return array_sum($valores);
   }
 
   echo soma(2, 2);
   echo "<br>";
   echo soma(2, 3);
   echo "<br>";
-  
+  echo soma(1.5, 2.3);
+  echo "<br>";
 ?>
