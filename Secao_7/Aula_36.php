84a7c40ece8743b1eb6db16e292a7c9f68c36aaf
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-26 14:48:56 +0000

    Aula_36 ->Funções Recursivas

--- Secao_7/Aula_36.php
+++ Secao_7/Aula_36.php
@@ -7,7 +7,7 @@
 				//Inicio: Diretor comercial
 				array(
 					'nome_cargo' => 'Diretor Comercial',
-					'sobordinado'=>array(
+					'sobordinados'=>array(
 
 						//Inicio: Gerente de Vendas
 						array('nome_cargo' => 'Gerente de Vendas'
@@ -56,7 +56,14 @@
 		$html = "<ul>";
 			
 			foreach ($cargos as $cargo) {
+					$html .= "<li>";
+					$html .= $cargo['nome_cargo'];
 					
+					if(isset($cargo['subordinados']) && count($cargo['subordinados']) > 0){
+						$html .= exibe();
+					}
+
+					$html .= "</li>";
 			}
 
 		$html .= "</ul>";
