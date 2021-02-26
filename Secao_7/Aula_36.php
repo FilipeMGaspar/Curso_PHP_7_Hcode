090f4640559917fd23059d79d84c9a821d0bde10
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-26 13:59:23 +0000

    Aula_36 ->Funções Recursivas

--- Secao_7/Aula_36.php
+++ Secao_7/Aula_36.php
@@ -1,22 +1,30 @@
 <?php
 	$hiarquia = array(
 		array(
-			'nome_cargo' => 'CEO'
+			'nome_cargo' => 'CEO',
 			'subordinados' => array(
 
 				//Inicio: Diretor comercial
 				array(
-					'nome_cargo' => 'Diretor Comercial'
+					'nome_cargo' => 'Diretor Comercial',
 					'sobordinado'=>array(
 
 						//Inicio: Gerente de Vendas
-						array('nome_cargo' => 'Gerente de Vendas')
+						array('nome_cargo' => 'Gerente de Vendas'
+						)
 					)
 					//Fim: Gerente de Vendas
 				),
 				//Fim: Diretor Comercial 
 
-				array()
+				//Inicio: Dirictor Financeiro
+				array(
+					'nome_cargo' =>'Dirictor Financeiro',
+					'subordinados' => array(
+						
+					)
+				)
+				//Fim: Dirictor Financeiro
 			)
 		)
 	);
