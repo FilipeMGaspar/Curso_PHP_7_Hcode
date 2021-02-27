8cd18073e682df79274352aadec33fba06d8c3f8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-27 16:49:43 +0000

    Aula_43 -> Atributos e Métodos

--- Secao_9/Aula_43.php
+++ Secao_9/Aula_43.php
@@ -5,6 +5,11 @@
 		private $motor;
 		private $ano;
 
-		
+		public function getModelo(){
+			return $this->modelo;
+		}
+		public function setModelo($modelo){
+			$this->modelo = $modelo;
+		}
 	}
 ?>
