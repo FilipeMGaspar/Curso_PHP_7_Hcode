075a48aaabea2b7804af40fcadd13cbbe3c76123
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-27 16:26:07 +0000

    Aula_41 ->Conceitos de Programação Orientada a Objetos

--- Secao_9/Aula_41.php
+++ Secao_9/Aula_41.php
@@ -1,3 +1,16 @@
 <?php
-	
-?>
+	//O que tenho
+		//Atributos
+			//Base de Dados
+				//Utilizador
+				//Senha
+
+	//O que faço
+		//Métodos
+			//Ligar
+			//Desligar
+
+	//Como estou
+		//Sempre no momento
+
+?>	
