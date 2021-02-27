c005021a4116a25830965854eb9a614279d5f301
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-27 16:52:17 +0000

    Aula_43 -> Atributos e Métodos

--- Secao_9/Aula_43.php
+++ Secao_9/Aula_43.php
@@ -1,15 +1,19 @@
 <?php
 	
 	class Carro{		
-		private $modelo;
+		private $modelo; //Atributo da class
 		private $motor;
 		private $ano;
 
+		//get e set do $modelo
 		public function getModelo(){
 			return $this->modelo;
 		}
 		public function setModelo($modelo){
-			$this->modelo = $modelo;
+			$this->modelo = $modelo;//$this->modelo referente ao atributo da class $modelo refere á variavel da função
 		}
+
+		//get e set do $motor
+		
 	}
 ?>
