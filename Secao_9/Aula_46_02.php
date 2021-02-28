2a3329fc71116a64803eac93dd74821170d88510
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-28 17:50:14 +0000

    Aula_46 -> Encapsulamento

--- Secao_9/Aula_46_02.php
+++ Secao_9/Aula_46_02.php
@@ -19,7 +19,11 @@ ini_set("display_errors", 1);
 	}
 
 	class Programador extends Pessoa {
+		
 		public function verDados(){
+			
+			echo get_class($this)."<br><br>";
+
 			echo $this->nome . "<br>";
 			echo $this->idade."<br>";
 			echo $this->senha."<br>";
