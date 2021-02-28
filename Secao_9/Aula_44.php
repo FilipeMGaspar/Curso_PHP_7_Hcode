6fdada47c4e4734ccdbad1f8f47c89a9db931f9d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-28 15:55:17 +0000

    Aula_44 -> Métodos Estáticos

---
+++ Secao_9/Aula_44.php
@@ -0,0 +1,12 @@
+<?php
+	class Documento{
+		private $numero:
+
+		public function getNumero(){
+			return $this->numero;
+		}
+		public function setNumero($numero){
+			$this->numero = $numero; 
+		}
+	}
+?>
