d1d55c94993e66d0b5ef4f41e1ffc319b4445ed0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-28 17:37:09 +0000

    Aula_46 -> Encapsulamento

--- Secao_9/Aula_46.php
+++ Secao_9/Aula_46.php
@@ -3,5 +3,11 @@
 		public $nome = "Rasmos Lerdof";
 		protected $idade = 48;
 		private $senha = '123456';
+
+		public function verDados(){
+			echo $this->nome . "<br>";
+			echo $this->idade."<br>";
+			echo $this->senha."<br>";
+		}
 	}
 ?>
