5061565d747243da79b24b874da968f8adf95908
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-28 17:47:23 +0000

    Aula_46 -> Encapsulamento

---
+++ Secao_9/Aula_46_02.php
@@ -0,0 +1,32 @@
+<?php
+/* Informa o nível dos erros que serão exibidos */
+error_reporting(E_ALL);
+ 
+/* Habilita a exibição de erros */
+ini_set("display_errors", 1);
+
+
+	class Pessoa{
+		public $nome = "Rasmus Lerdorf";
+		protected $idade = 48;
+		private $senha = '123456';
+
+		public function verDados(){
+			echo $this->nome . "<br>";
+			echo $this->idade."<br>";
+			echo $this->senha."<br>";
+		}
+	}
+
+	class Programador extends Pessoa {
+
+	}
+
+	$objecto = new Programador();
+	//echo $objecto->nome."<br>";
+	//echo $objecto->idade. "<br>";
+	//echo $objecto->senha."<br>";
+	
+	$objecto->verDados();
+
+?>
