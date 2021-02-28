251d0c9dc0a2b0a824721ed60188f6bd137cbf87
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-28 16:06:13 +0000

    Aula_44 -> Métodos Estáticos

--- Secao_9/Aula_44.php
+++ Secao_9/Aula_44.php
@@ -8,7 +8,20 @@
 		public function setNumero($numero){
 			$this->numero = $numero; 
 		}
+
+		//Função para validar o CPF
+		public static function validar($cpf){
+			//Verifica se o CPF foi informado
+			if(empty($cpf)){
+				return false;
+			}
+
+			// Elimina possivel mascara
+			$cpf = preg_replace('[^0-9]', '', $cpf);
+			$cpf = str_pad($cpf, 11,'0', STR_PAD_LEFT);
+		}
 	}
+
 
 	$cpf = new Documento();
 	$cpf->setNumero("32165498712");
