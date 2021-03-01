922d5e47661ff6619a625d9a58148f3486595b0c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-01 10:24:44 +0000

    Aula_49 -> Classe Abstrata

---
+++ Secao_9/Aula_49.php
@@ -0,0 +1,29 @@
+<?php
+	/* Informa o nível dos erros que serão exibidos */
+error_reporting(E_ALL);
+ 
+/* Habilita a exibição de erros */
+ini_set("display_errors", 1);
+
+	interface Veiculo {
+
+		public function acelerar($velocidade);
+		public function travar($velocidade);
+		public function trocarMarcha($marcha);
+	}
+
+	abstract class Automovel implements Veiculo {
+		public function acelerar($velocidade){
+			echo "O Veiculo acelerou até: ".$velocidade." Km/h";
+		}
+		public function travar($velocidade){
+			echo "O veículo travou até: " . $velocidade." Km/h";
+		}
+
+		public function trocarMarcha($marcha){
+			echo "O Veiculo engatou a marcha: ".$marcha;
+		}
+	}
+
+	
+?>
