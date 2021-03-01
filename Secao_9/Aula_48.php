4a5518900d45608a3144e369a3290dc44682b23c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-01 10:09:02 +0000

    Aula_48 -> Interface

--- Secao_9/Aula_48.php
+++ Secao_9/Aula_48.php
@@ -1,8 +1,20 @@
 <?php
+/* Informa o nível dos erros que serão exibidos */
+error_reporting(E_ALL);
+ 
+/* Habilita a exibição de erros */
+ini_set("display_errors", 1);
+
 	interface Veiculo {
 
 		public function acelerar($velocidade);
 		public function travar($velocidade);
 		public function TrocarMarcha($marcha);
 	}
+
+	class Civic implements Veiculo {
+
+	}
+
+	$carro = new Civic();
 ?>
