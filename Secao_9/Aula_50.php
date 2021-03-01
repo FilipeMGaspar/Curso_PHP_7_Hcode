ba5ce791ff623ee702b1e92d87c167e33ff660bd
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-01 10:52:47 +0000

    Aula_50 -> Polimorfismo

--- Secao_9/Aula_50.php
+++ Secao_9/Aula_50.php
@@ -1,18 +1,7 @@
 <?php
 
-
 	require_once "../mostraerros.php";
 
-	interface Teste{
-		public function falar($fala);
-	}
+	
 
-	class Testar implements Teste{
-		public function falar($fala){
-			echo $fala;
-		}
-	}
-
-	$fala = new Testar();
-	echo $fala->falar("Estou a falar! <h2>Estejam atentos!</h2>");
 ?>
