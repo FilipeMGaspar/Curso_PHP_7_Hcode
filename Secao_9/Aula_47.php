07447ce01b570da4cc0570f13384df6eb0a88e2c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-01 08:56:48 +0000

    Aula_47 -> Herança

--- Secao_9/Aula_47.php
+++ Secao_9/Aula_47.php
@@ -1,4 +1,10 @@
 <?php
+/* Informa o nível dos erros que serão exibidos */
+error_reporting(E_ALL);
+ 
+/* Habilita a exibição de erros */
+ini_set("display_errors", 1);
+
 	class Documento {
 		private $numero;
 
@@ -18,5 +24,10 @@
 		}
 	}
 
-	
+	$doc = new CPF();
+	$doc->setNumero('112233445566');
+
+	var_dump($doc->validar());
+	echo "<br><br>";
+	echo $doc->getNumero();
 ?>
