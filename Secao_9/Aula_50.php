02f2c208850dcfb1500c44870593f2940bb46d04
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-01 10:49:39 +0000

    Aula_50 -> Polimorfismo

--- Secao_9/Aula_50.php
+++ Secao_9/Aula_50.php
@@ -1,20 +1,18 @@
 <?php
-	/* Informa o nível dos erros que serão exibidos */
-error_reporting(E_ALL);
- 
-/* Habilita a exibição de erros */
-ini_set("display_errors", 1);
 
-	//require_once "../mostraerros.php";
+
+	require_once "../mostraerros.php";
 
 	interface Teste{
 		public function falar($fala);
 	}
 
 	class Testar implements Teste{
-
+		public function falar($fala){
+			echo $fala;
+		}
 	}
 
 	$fala = new Testar();
-	//echo $fala;
+	echo $fala->falar("Estou a falar! Estejam atentos");
 ?>
