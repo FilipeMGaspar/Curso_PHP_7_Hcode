b627f35d2ab0d20f1215ae95b343bc5f73f6de86
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-01 08:46:36 +0000

    Aula_47 -> Herança

---
+++ Secao_9/Aula_47.php
@@ -0,0 +1,13 @@
+<?php
+	class Documento {
+		private $numero;
+
+		public function getNumero(){
+			return $this->numero;
+		}
+		public function setNumero($num){
+			$this->numero = $num;
+		}
+
+	}
+?>
