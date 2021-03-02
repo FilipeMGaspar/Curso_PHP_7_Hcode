a3c0e7ea9c834f4279a4bd349b87cd95df769ff2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-02 16:06:07 +0000

    Aula_52 -> Usando Namespace

--- Secao_9/aula_52/Aula_52.php
+++ Secao_9/aula_52/Aula_52.php
@@ -1,5 +1,13 @@
 <?php
 	require_once "../../mostraerros.php";
 
-	
+	require_once("config.php");
+
+	$cad = new Cadastro();
+
+	$cad->setNome("Djalma Sinddeaux");
+	$cad->setEmail("djalma@hcode.br");
+	$cad->setSenha("123456");
+
+	echo $cad;
 ?>
