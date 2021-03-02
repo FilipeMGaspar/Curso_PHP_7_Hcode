cb8c87ffe1b20bae9c26e1a58c45e6d4b8a6096d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-02 16:16:07 +0000

    Aula_52 -> Usando Namespace

--- Secao_9/aula_52/config.php
+++ Secao_9/aula_52/config.php
@@ -5,7 +5,6 @@
 		$dirClass = "Classe";
 		$fileName = $dirClass. DIRECTORY_SEPARATOR . $nameClass .".php";
 
-		echo $fileName."<br>";
 		if(file_exists($fileName)) {
 			require_once "$fileName";
 		}
