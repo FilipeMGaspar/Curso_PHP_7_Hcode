d8bdbc992541cd4d7e655a6d6149fbee13502c5f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-02 16:15:20 +0000

    Aula_52 -> Usando Namespace

--- Secao_9/aula_52/config.php
+++ Secao_9/aula_52/config.php
@@ -1,12 +1,13 @@
 <?php
 	require_once "../../mostraerros.php";
 
-	spl_autoload_register(function($nameClass){
+	spl_autoload_register(function($nameClass) {
 		$dirClass = "Classe";
-		$fileName = $dirClass. DIRECTORY_SEPARATOR . $nameClass.".php";
+		$fileName = $dirClass. DIRECTORY_SEPARATOR . $nameClass .".php";
 
-		if(file_exists($fileName) {
-			require_once($fileName);
+		echo $fileName."<br>";
+		if(file_exists($fileName)) {
+			require_once "$fileName";
 		}
 	});
 
