c3c9c7008e075f3e4c9085321c63cb20f4fad35a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-02 17:46:38 +0000

    Aula_52 -> Usando Namespace

--- Secao_9/aula_52/Classe/Cliente/Cadastro.php
+++ Secao_9/aula_52/Classe/Cliente/Cadastro.php
@@ -1,7 +1,8 @@
 <?php
-	namespace Cliente;
+	
+	namespace cliente;
 
-	class Cadastro extends \Cadastro {
+	class Cadastro extends Cadastro {
 
 		public function registarVenda(){
 			echo "Foi feito o registo de uma venda para o cliente ". $this->getNome();
