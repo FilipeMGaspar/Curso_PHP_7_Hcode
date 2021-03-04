484110aaef1f03a829e51237d300ea461ca6bc93
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-04 13:26:39 +0000

    Aula_55 -> Classe MySQLi e o PHP

--- Secao_11/Aula_55.php
+++ Secao_11/Aula_55.php
@@ -1,22 +1,26 @@
 <?php
 	//Incluir o mostrar erros
-	require_once "../mostraerros.php" 
+	/* Informa o nível dos erros que serão exibidos */
+		error_reporting(E_ALL);
+ 
+	/* Habilita a exibição de erros */
+		ini_set("display_errors", 1);
 
-	$conect = new mysqli("localhost", "admin", "123", "db_php7");
+
 	//$conect = new mysqli("localhost", "admin", "123a", "db_php7");
-
+	$conect = new mysqli("localhost", "admin", "123", "db_php7");
 
 	if($conect->connect_error){
 		echo "Erro: " . $conect->connect_error;
 	}
 
-	$stmt = $conect->prepare("insert into tb_usuarios (deslogin, dessenha) values(?, ?)");
+	$stmt = $conect->prepare("INSERT INTO tb_usuarios (deslogim, dessenha) VALUES(?, ?)");
 
-	$stmt -> bind_param("ss", $login, $pass);
+	$stmt->bind_param("ss", $login, $pass);
 
 	
 	$login = "user";
 	$pass = "123456";
 
-	$stmt->executar(); 
+	$stmt->execute(); 
 ?>
