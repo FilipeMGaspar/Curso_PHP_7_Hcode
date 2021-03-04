d0fec3c631d0d782465f905ff051ef3670afc4b1
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-04 12:59:45 +0000

    Aula_55 -> Classe MySQLi e o PHP 7

--- Secao_11/Aula_55.php
+++ Secao_11/Aula_55.php
@@ -1,5 +1,7 @@
 <?php
-	$conect = new mysqli("localhost", "admin", "123", "db_php7");
+	//$conect = new mysqli("localhost", "admin", "123", "db_php7");
+	$conect = new mysqli("localhost1", "admin", "123", "db_php7");
+
 
 	if($conect->connect_error){
 		echo "Erro: " . $conect->connect_error;
