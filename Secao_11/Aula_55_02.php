c170576c71fb711145ed0af75f5329943b86fc36
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-04 13:36:25 +0000

    Aula_55 -> Classe MySQLi e o PHP

---
+++ Secao_11/Aula_55_02.php
@@ -0,0 +1,15 @@
+<?php
+	//Incluir o mostrar erros
+	require_once "../mostraerros.php";
+
+	//Coneção á base de dados através de Mysqli
+	$conect = new mysqli("localhost", "admin", "123", "db_php7");
+
+	//Testa se a conreção á base de dados foi bem sucedida
+	if($conect->connect_error){
+		echo "Erro: " . $conect->connect_error;
+	}
+
+	//
+
+?>
