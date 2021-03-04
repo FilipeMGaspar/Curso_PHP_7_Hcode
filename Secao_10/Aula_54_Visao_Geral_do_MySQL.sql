71c33e7e093b3f39262c53519d17f24c6cae3357
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-04 08:53:13 +0000

    Aula_54 -> Visão Geral do MySQL

--- Secao_10/Aula_54_Visao_Geral_do_MySQL.sql
+++ Secao_10/Aula_54_Visao_Geral_do_MySQL.sql
@@ -1,1 +1,8 @@
 use db_php7;
+
+create  table tb_usuarios(
+	idusuario int not null auto_increment primary key,
+    deslogim varchar(64) not null,
+    dessenha varchar(256) not null,
+    dtcadastro timestamp not null default current_timestamp()
+);
