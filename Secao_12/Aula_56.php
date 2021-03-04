5f0527f6d965c5478fc1ffb66c0e8c4afb4d4df5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-04 14:40:09 +0000

    Aula_56 -> Conectando o PHP 7 com MySQL usando PDO

--- Secao_12/Aula_56.php
+++ Secao_12/Aula_56.php
@@ -2,5 +2,10 @@
 	//Incluir o mostrar erros
 	require_once "../mostraerros.php";
 
+	//coneção ao banco de dados em pdo
 	$conect = new PDO("mysql:dbname=db_php7;host=localhost", "admin", "123");
+
+	$stmt = $conect->prepare("SELECT * FROM tb_usuarios ORDER BY deslogim");
+
+	$stmt->execute();
 ?>
