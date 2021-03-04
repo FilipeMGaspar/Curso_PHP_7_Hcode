8988014324d0bf9f63a456d4ebecdac9bc8fbc89
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-04 13:03:51 +0000

    Aula_55 -> Classe MySQLi e o PHP

--- Secao_11/Aula_55.php
+++ Secao_11/Aula_55.php
@@ -1,9 +1,13 @@
 <?php
-	//$conect = new mysqli("localhost", "admin", "123", "db_php7");
-	$conect = new mysqli("localhost", "admin", "123a", "db_php7");
+	$conect = new mysqli("localhost", "admin", "123", "db_php7");
+	//$conect = new mysqli("localhost", "admin", "123a", "db_php7");
 
 
 	if($conect->connect_error){
 		echo "Erro: " . $conect->connect_error;
 	}
+
+	$stmt = $conect->prepare("insert into tb_usuarios (deslogin, dessenha) values(?, ?)");
+
+	$stmt->bind_param("ss"); 
 ?>
