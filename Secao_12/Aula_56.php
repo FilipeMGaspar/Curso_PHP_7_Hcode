cc6a9f097c081a74cb5ae5700a059e7596f93f2f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-04 14:44:56 +0000

    Aula_56 -> Conectando o PHP 7 com MySQL usando PDO

--- Secao_12/Aula_56.php
+++ Secao_12/Aula_56.php
@@ -13,4 +13,11 @@
 
 	var_dump($results);
 
+	foreach ($results as $row) {
+		foreach ($row as $key => $value) {
+			echo "<strong>".$key ."</strong>" .$value . "<br>";
+		}
+		echo "*******************************<br>";
+	}
+
 ?>
