695b3790ebdecc195732d814c75cbe69d97c08f0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-05 18:36:34 +0000

    Aula_58 -> Conectando o PHP 7 com SQL Server usando PDO

--- Secao_12/Aula_58.php
+++ Secao_12/Aula_58.php
@@ -2,8 +2,8 @@
 	//Incluir o mostrar erros
 	require_once "../mostraerros.php";
 
-	//coneção ao banco de dados em pdo
-	$conect = new PDO("mysql:dbname=db_php7;host=localhost", "admin", "123");
+	//coneção ao banco de dados em pdo ao Microsoft SQLSERVER 
+	$conect = new PDO("sqlsrv:Database=db_php7;server=localhost\SQLExpress;ConnectionPooling=0", "sa", "123456");
 
 	
 
