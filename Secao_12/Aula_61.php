2125e1676ed60d90238a113a6281a2f2ae3cc166
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-06 18:14:45 +0000

    Aula_61 -> PDO - Alterando dados no banco

--- Secao_12/Aula_61.php
+++ Secao_12/Aula_61.php
@@ -5,6 +5,14 @@
 	//coneção ao banco de dados em pdo
 	$conect = new PDO("mysql:dbname=db_php7;host=localhost", "admin", "123");
 
+	/*$login = "Jose";
+	$passaword = "1234567890";
+	$id = 2;
+
+	$sql = "UPDATE tb_usuarios SET deslogim=?, desenha=? WHERE idusuario=?";
+	$stmt= $conect->prepare($sql);
+	$stmt->execute([$login, $passaword, $id]);*/
+	
 	$stmt = $conect->prepare("UPDATE tb_usuarios SET deslogim = :LOGIN , desenha= :PASSWORD WHERE idusuario = :ID");
 
 	$login = "Jose";
@@ -16,7 +24,7 @@
 	$stmt->bindParam(":ID", $id);
 
 	$stmt->execute();
-
+	
 	echo "Dados alterados com sucesso!";
 
 ?>
