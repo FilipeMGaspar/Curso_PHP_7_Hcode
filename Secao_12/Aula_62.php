50e02a2839e729cac5f550bf347a431fb03d2395
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-06 19:59:51 +0000

    Aula_62 -> PDO: Apagando dados no banco

--- Secao_12/Aula_62.php
+++ Secao_12/Aula_62.php
@@ -1,4 +1,15 @@
 <?php
 	//Incluir script que permite mostrar os erros
 	require_once "../mostraerros.php";
+
+	//coneção com a base de dados MySql através de PDO
+	$conect = new PDO("mysql:dbname=db_php7;host=localhost", "admin", "123");
+
+	$stmt = $conect->prepare("DELETE FROM tb_usuarios WHERE 'idusuario'= :ID");
+
+	$id = 1;
+
+	$stmt->bindParam(":ID", $id);
+
+	$stmt->execute();
 ?>
