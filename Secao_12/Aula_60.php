974015bd82ea12eb536bf77bb83fbb71cf2d9c30
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-06 20:21:31 +0000

    Aula_60 -> PDO: Inserindo dados no banco

--- Secao_12/Aula_60.php
+++ Secao_12/Aula_60.php
@@ -5,7 +5,7 @@
 	//coneção ao banco de dados em pdo
 	$conect = new PDO("mysql:dbname=db_php7;host=localhost", "admin", "123");
 
-	$stmt = $conect->prepare("INSERT INTO tb_usuarios (deslogim, desenha) VALUES (:LOGIN, :PASSWORD)");
+	$stmt = $conect->prepare("INSERT INTO tb_usuarios (deslogim, dessenha) VALUES (:LOGIN, :PASSWORD)");
 
 	$login = "Jose";
 	$passaword = "1234567890";
