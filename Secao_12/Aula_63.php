ab94693815e00a11795496f7bc5a5932849ea0a8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-06 20:08:08 +0000

    Aula_63 -> PDO: Usando transações

--- Secao_12/Aula_63.php
+++ Secao_12/Aula_63.php
@@ -1,4 +1,20 @@
 <?php
 	//Incluir script que permite mostrar os erros
 	require_once "../mostraerros.php";
+
+		//coneção com a base de dados MySql através de PDO
+	$conect = new PDO("mysql:dbname=db_php7;host=localhost", "admin", "123");
+
+	$conect->beginTransaction();
+
+	$stmt = $conect->prepare("DELETE FROM tb_usuarios WHERE idusuario= ?");
+
+	$id = 1;
+
+	$stmt->execute(array($id));
+
+	$conect->rollback();
+
+	echo "Dodos eliminados com sucesso!";
+?>
 ?>
