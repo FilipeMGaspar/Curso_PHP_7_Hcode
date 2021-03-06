51486e248ccd1b960624ad986e439da92cea0693
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-06 17:32:27 +0000

    Aula_60 -> Conectando o PHP 7 com SQL Server usando PDO

--- Secao_12/Aula_60.php
+++ Secao_12/Aula_60.php
@@ -6,5 +6,9 @@
 	$conect = new PDO("mysql:dbname=db_php7;host=localhost", "admin", "123");
 
 	$stmt = $conect->prepare("INSERT INTO tb_usuarios (deslogim, desenha) VALUES (:LOGIN, :PASSWORD)");
-	
+
+	$login = "Jose";
+	$passaword = "1234567890";
+
+	$stmt->bindParam();
 ?>
