37b3387e3fdd2c190f313c8a95e6e136e72a57db
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-07 09:34:06 +0000

    Aula_64 -> PDO - DAO - Data Access Object

--- Secao_13/Aula_64.php
+++ Secao_13/Aula_64.php
@@ -3,12 +3,4 @@
 	require_once "../mostraerros.php";
 	//Aula_64 -> PDO - DAO - Data Access Object
 
-	spl_autoload_register(function($class_name){
-
-		$filename = $class_name.".php";
-
-		if(file_exists($filename)){
-			require_once($filename);
-		}
-	});
 ?>
