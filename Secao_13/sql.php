95aced8367ead041edcab95fa67e836e6c3976cc
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-07 09:18:05 +0000

    Aula_64 -> PDO - DAO - Data Access Object

--- Secao_13/sql.php
+++ Secao_13/sql.php
@@ -7,16 +7,25 @@ require_once "../mostraerros.php";
 
 		private $conn;
 
+		//Método construtor responsável pela ligação á base de dados
 		public function __construct(){
 			$this->conn = new PDO("mysql:host=localhost;dbname=db_php7, 'admin', '123'"); 
 		}
 
+		//Método privado para relacionar os dados recebidos com os valores a serem inseridos na tabela
+		private function setParams($statment, $parameters = array()){
+			foreach ($parameters as $key => $value) {
+				$statment->bindParam($key, $value);
+			}
+		}
+
+		//Método resposável por preparar as ações a serem efectuadas no banco de dados: INSERT; UPDATE; DELETE; ....
 		public function query($rawQuery, $params = array()){
 
 			$stmt = $this->conn->prepare($RawQuery);
 
-			foreach ($variable as $key => $value) {
-				
+			foreach ($params as $key => $value) {
+				$stmt->bindParam($key, $value);
 			}
 		}
 	}
