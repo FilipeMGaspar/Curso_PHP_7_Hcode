e6d25fefc9aa68c47aae4c13918eca2d221e4335
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-07 09:35:37 +0000

    Aula_64 -> PDO - DAO - Data Access Object

--- Secao_13/Aula_64.php
+++ Secao_13/Aula_64.php
@@ -3,4 +3,9 @@
 	require_once "../mostraerros.php";
 	//Aula_64 -> PDO - DAO - Data Access Object
 
+	require_once "config.php";
+
+	$sql = new Sql();
+
+	$sql->select("SELECT * FROM tb_usuarios");
 ?>
