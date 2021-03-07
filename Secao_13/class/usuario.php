85d1f1db2dc3b05a9ef74ef28976d2dd69aa991d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-07 10:09:58 +0000

    Aula_65 -> PDO - DAO - SELECT ; DAO -> Data Access Object

--- Secao_13/class/usuario.php
+++ Secao_13/class/usuario.php
@@ -13,21 +13,29 @@ require_once "../../mostraerros.php";
 	public function getIdusuario(){
 		return $this->idusuario;
 	}
+	public function setIdusuario($IdUser){
+		$this->idusuario = $IdUser;
+	}
 
 	public function getDeslogim(){
 		return $this->deslogim;
+	}
+	public function setDeslogim($login){
+		$this->deslogim = $login;
 	}
 
 	public function getDessenha(){
 		return $this->dessenha;
 	}
+	public function setDessenha($senha){
+		$this->dessenha = $senha;
+	}
 	
 	public function getDtcadastro(){
 		return $this->dtcadastro;
+	}	
+	public function setDtcadastro($DtCadastro){
+		$this->dtcadastro = $DtCadastro;
 	}
 
-	//public function
-	//public function
-	//public function
-	//public function
 ?>
