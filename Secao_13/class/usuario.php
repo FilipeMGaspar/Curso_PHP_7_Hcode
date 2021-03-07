aef5d9b0c540152c078c96653ca8c348757f5d0d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-07 10:17:27 +0000

    Aula_65 -> PDO - DAO - SELECT ; DAO -> Data Access Object

--- Secao_13/class/usuario.php
+++ Secao_13/class/usuario.php
@@ -8,38 +8,45 @@ require_once "../../mostraerros.php";
 		private $deslogim;
 		private $dessenha;
 		private $dtcadastro;
-	}
+	
 
 	
-	public function getIdusuario(){
-		return $this->idusuario;
-	}
-	public function setIdusuario($IdUser){
-		$this->idusuario = $IdUser;
-	}
+		public function getIdusuario(){
+			return $this->idusuario;
+		}
+		public function setIdusuario($IdUser){
+			$this->idusuario = $IdUser;
+		}
 
 
-	public function getDeslogim(){
-		return $this->deslogim;
-	}
-	public function setDeslogim($login){
-		$this->deslogim = $login;
-	}
+		public function getDeslogim(){
+			return $this->deslogim;
+		}
+		public function setDeslogim($login){
+			$this->deslogim = $login;
+		}
 
 	
-	public function getDessenha(){
-		return $this->dessenha;
-	}
-	public function setDessenha($senha){
-		$this->dessenha = $senha;
-	}
+		public function getDessenha(){
+			return $this->dessenha;
+		}
+		public function setDessenha($senha){
+			$this->dessenha = $senha;
+		}
 	
 	
-	public function getDtcadastro(){
-		return $this->dtcadastro;
-	}	
-	public function setDtcadastro($DtCadastro){
-		$this->dtcadastro = $DtCadastro;
-	}
+		public function getDtcadastro(){
+			return $this->dtcadastro;
+		}	
+		public function setDtcadastro($DtCadastro){
+			$this->dtcadastro = $DtCadastro;
+		}
 
+		public function loadById($id){
+			$sql = new sql();
+			$result = sql->select("SELECT * FROM tb_usuarios WHERE idusuario= :ID", array(
+				":ID"=>$id
+			)):
+		}
+	}
 ?>
