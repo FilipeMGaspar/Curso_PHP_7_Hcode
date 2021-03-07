7472ab3bac6de21af0f62268e15b748b36ebb9a3
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-07 10:04:49 +0000

    Aula_65 -> PDO - DAO - SELECT ; DAO -> Data Access Object

--- Secao_13/class/usuario.php
+++ Secao_13/class/usuario.php
@@ -1,3 +1,33 @@
 <?php
+//Incluir script que permite mostrar os erros
+require_once "../../mostraerros.php";
 
+	//Criação da class usuario
+	class Usuario{
+		private $idusuario;
+		private $deslogim;
+		private $dessenha;
+		private $dtcadastro;
+	}
+
+	public function getIdusuario(){
+		return $this->idusuario;
+	}
+
+	public function getDeslogim(){
+		return $this->deslogim;
+	}
+
+	public function getDessenha(){
+		return $this->dessenha;
+	}
+	
+	public function getDtcadastro(){
+		return $this->dtcadastro;
+	}
+
+	//public function
+	//public function
+	//public function
+	//public function
 ?>
