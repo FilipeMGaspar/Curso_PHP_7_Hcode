dd72a9fe8231a884e07c4fe605bb31e35b09f355
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-09 11:13:02 +0000

    Aula_71 -> Criando arquivos com fopen()

--- Secao_14/Aula_71_02.php
+++ Secao_14/Aula_71_02.php
@@ -2,6 +2,19 @@
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
 
-	
+	require_once "config.php";
 
+	$sql = new sql();
+
+	$usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogim");
+
+	//print_r($usuarios);
+
+	$headers = array();
+
+	foreach ($usuarios[0] as $key => $value) {
+		array_push($headers, ucfirst($key));
+	}
+
+	print_r($headers);
 ?>
