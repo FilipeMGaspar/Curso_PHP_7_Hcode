880e12e3f264a1f10f49ba28fb49f50549c4d175
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-09 09:43:04 +0000

    Aula_70 -> Lendo e manipulando pastas/diretórios com PHP 7

--- Secao_14/Aula_70_02.php
+++ Secao_14/Aula_70_02.php
@@ -11,16 +11,21 @@
 			$filename = 'img' . DIRECTORY_SEPARATOR . $img;
 
 			var_dump($filename);
-			echo "<br><br>"
+			echo "<br><br>";
 
 			$info = pathinfo($filename);
 
 			$info["size"] = filesize($filename);
 
 			$info["modifed"] = date("d/m/Y H:i:s", fileatime($filename));
-			//$info['url'] = "http://192.168.0.250/Curso_PHP_7_Hcode/Secao_14/img/";
+			
+			$info['url'] = "http://192.168.0.250/Curso_PHP_7_Hcode/Secao_14/img/" . $filename;
 
-			$info['url'] = "http://192.168.0.250/Curso_PHP_7_Hcode/Secao_14/img/" . str_replace("\/\/" , "/", $filename);
+			echo "<br><br>";
+			var_dump($info);
+			echo "<br><br>";
+
+			//$info['url'] = "http://192.168.0.250/Curso_PHP_7_Hcode/Secao_14/img/" . str_replace("\/\/" , "/", $filename);
 
 			//var_dump($info);
 
