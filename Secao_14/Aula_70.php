bd98737e106960e7e3dc74535419722f4bac28cb
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-09 08:38:58 +0000

    Aula_70 -> Lendo e manipulando pastas/diretórios com PHP 7

--- Secao_14/Aula_70.php
+++ Secao_14/Aula_70.php
@@ -2,5 +2,10 @@
 	//Incluir script que permite mostrar os erros
 	require_once "../mostraerros.php";
 
-	
+	$name = "images";
+
+	//verifica se a pasta existe
+	if(!is_dir($name)){
+		mkdir($name);
+	}
 ?>
