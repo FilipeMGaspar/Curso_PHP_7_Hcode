d66d36dce21b7af3224976c56f182265a91661c7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-09 10:34:04 +0000

    Aula_71 -> Criando arquivos com fopen()

--- Secao_14/Aula_71.php
+++ Secao_14/Aula_71.php
@@ -2,5 +2,7 @@
 	//Incluir script que permite mostrar os erros
 	require_once "../mostraerros.php";
 
-	
+	$file = fopen("log.txt", "w+");//caminho do arquivo, modo de abertura leitura, escrita
+
+	fwrite($file, date());
 ?>
