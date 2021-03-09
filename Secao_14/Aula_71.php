36260461ff049aabfd9744b763922138773f9ba1
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-09 10:36:02 +0000

    Aula_71 -> Criando arquivos com fopen()

--- Secao_14/Aula_71.php
+++ Secao_14/Aula_71.php
@@ -5,4 +5,8 @@
 	$file = fopen("log.txt", "w+");//caminho do arquivo, modo de abertura leitura, escrita
 
 	fwrite($file, date("d/m/Y H:i:s"));
+
+	fclose($file);
+
+	echo "Arquivo criado com sucesso!";
 ?>
