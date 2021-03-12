95be41232eeb18784f0e0f8559682093f3ee4049
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-12 15:43:40 +0000

    Aula_76 -> Movendo um arquivo

--- Secao_14/Aula_76.php
+++ Secao_14/Aula_76.php
@@ -15,7 +15,7 @@ require_once "../mostraerros.php";
 
 	$filename = "README.txt";
 
-	if(file_exists($dir1 . DIRECTORY_SEPARATOR . $filename)){
+	if(!file_exists($dir1 . DIRECTORY_SEPARATOR . $filename)){
 		$file = fopen($dir1 . DIRECTORY_SEPARATOR . $filename, "w+");
 
 		fwrite($file, date("d/m/Y H:i:s"));
