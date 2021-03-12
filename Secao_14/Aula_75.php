d0a2da23b019ec8f2822c936ea9da44880f4d158
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-12 13:12:55 +0000

    Aula_75 -> Download de arquivos com file_get_contents

--- Secao_14/Aula_75.php
+++ Secao_14/Aula_75.php
@@ -5,6 +5,8 @@ require_once "../mostraerros.php";
 	$link = "https://www.google.pt/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
 
 	$content = file_get_contents($link);
+	//var_dump($content);
 
-	var_dump($content);
+	$parse = parse_url($link);
+	var_dump($parse);
 ?>
