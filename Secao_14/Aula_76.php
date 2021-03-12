a085c79efd04314a77cde284ede75381933f0f00
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-12 15:34:53 +0000

    Aula_76 -> Movendo um arquivo

--- Secao_14/Aula_76.php
+++ Secao_14/Aula_76.php
@@ -1,6 +1,15 @@
 <?php
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
+	
+	$dir1 = "folder01";
+	$dir2 = "folder02";
 
+	if(!is_dir($dir1)){
+		mkdir($dir1);
+	}
 
+	if(!is_dir($dir2)){
+		mkdir($dir2);
+	}
 ?>
