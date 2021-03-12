d38f3cb0b13dd279c1bb0b397f19c46bf4493472
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-12 16:10:19 +0000

    Aula_77 -> Usando a cURL

--- Secao_14/Aula_77.php
+++ Secao_14/Aula_77.php
@@ -1,5 +1,10 @@
 <?php
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
+	
+	//https://viacep.com.br/ws/01310100/json/
+	
+	$cep = "01310100";
 
+	$link = "https://viacep.com.br/ws/$cep/json/";
 ?>
