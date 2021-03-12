444a6bb991ab0155d5d1bfa2a56e595e373d8045
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-12 16:40:24 +0000

    Aula_78 -> Usando Cookies

--- Secao_14/Aula_78_02.php
+++ Secao_14/Aula_78_02.php
@@ -2,7 +2,7 @@
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
 
-	if(isset($_COOKIES["NOME_DO_COOKIE"])){
-		var_dump($_COOKIES["NOME_DO_COOKIE"]);
+	if(isset($_COOKIE["NOME_DO_COOKIE"])){
+		var_dump($_COOKIE["NOME_DO_COOKIE"]);
 	}
 ?>
