57cf19e9102e267bae24f4a342468710ecc1180d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-12 12:51:05 +0000

    Aula_75 -> Download de arquivos com file_get_contents

--- Secao_14/Aula_75.php
+++ Secao_14/Aula_75.php
@@ -2,5 +2,7 @@
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
 
+	$link = "https://www.google.pt/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
 
+	
 ?>
