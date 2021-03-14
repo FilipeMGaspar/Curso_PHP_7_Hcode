0f6d7310b76bc2f17c43d786c0010bd4621a2e7c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-14 16:47:19 +0000

    Aula_79 -> Try e Catch

--- Secao_15/Aula_79_02.php
+++ Secao_15/Aula_79_02.php
@@ -2,4 +2,13 @@
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
 
+	function trataNome($nome){
+		if(!$name){
+			throw new Exception("Nenhum nome foi informado.", 1);
+			
+		}
+	}
+
+	echo ucfirst($nome)."<br>";
+
 ?>
