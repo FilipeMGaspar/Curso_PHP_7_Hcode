1468aac142d833a47699c40e6373e49da5035980
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-14 17:35:50 +0000

    Aula_80 -> Tratando erros

--- Secao_15/Aula_80_02.php
+++ Secao_15/Aula_80_02.php
@@ -1,6 +1,8 @@
 <?php
 //Incluir script que permite mostrar os erros
-require_once "../mostraerros.php";
+//require_once "../mostraerros.php";
+
+	error_reporting(E_ALL & ~E_NOTICE);
 	
 	$nome = $_GET["nome"];
 
