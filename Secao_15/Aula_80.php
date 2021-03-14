3046a3cde57a6fce3816e70b61f22a5bf494b082
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-14 17:21:09 +0000

    Aula_80 -> Tratando erros

--- Secao_15/Aula_80.php
+++ Secao_15/Aula_80.php
@@ -4,5 +4,12 @@ require_once "../mostraerros.php";
 
 	function error_handler($code, $message, $file, $line){
 
+		echo json_encode(array(
+			"code"=>$code,
+			"message"=>$message,
+			"line"=>$line,
+			"file"=>$file
+		));
+
 	}
 ?>
