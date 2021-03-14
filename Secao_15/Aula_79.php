4709042f8f61604dd2cc717b230b5a04f5a08ba5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-14 16:38:21 +0000

    Aula_79 -> Try e Catch

--- Secao_15/Aula_79.php
+++ Secao_15/Aula_79.php
@@ -6,6 +6,11 @@ require_once "../mostraerros.php";
 		throw new Exception("Houve um erro.", 400);
 		
 	} catch (Exception $e){
-
+		echo json_encode(array(
+			"message"=>$e->getMessage(),
+			"line"=>$e->getLine(),
+			"file"=>$e->getFile(),
+			"code"=>$e=>getCode()
+		));
 	}
 ?>
