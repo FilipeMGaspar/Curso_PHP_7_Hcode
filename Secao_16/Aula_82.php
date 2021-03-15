afa37a224eb92e28a11741275126cdf246a5b10a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-15 17:39:30 +0000

    Aula_82 -> Usando imagem existente (GD)

--- Secao_16/Aula_82.php
+++ Secao_16/Aula_82.php
@@ -1,5 +1,10 @@
 <?php
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
+	
+	$image = imagecreatefromjpeg("certificado.jpg");
 
+	$titleColor = imagecolorallocate($image, 0, 0, 0);
+
+	$gray = imagecolorallocate($image, 100, 100, 100);
 ?>
