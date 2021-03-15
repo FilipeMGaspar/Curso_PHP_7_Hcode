53775590836f9ec82b38db48afe5eb39b66c39c0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-15 17:59:40 +0000

    Aula_83 -> Fontes TTF (GD)

---
+++ Secao_16/Aula_83.php
@@ -0,0 +1,22 @@
+<?php
+//Incluir script que permite mostrar os erros
+require_once "../mostraerros.php";
+	
+	$image = imagecreatefromjpeg("certificado.jpg");
+
+	$titleColor = imagecolorallocate($image, 0, 0, 0);
+
+	$gray = imagecolorallocate($image, 100, 100, 100);
+
+	imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
+
+	imagestring($image, 5, 440, 350, "Divanei Aparecida", $titleColor);
+
+	imagestring($image, 3, 440, 370, utf8_decode("Concluído em: ").date("d/m/Y"), $titleColor);
+
+	header("Content-type: image/jpeg");
+
+	imagejpeg($image, "certificado-".date("d-m-Y").".jpg", 10);
+
+	imagedestroy($image);
+?>
