79fcbacffb5337cf7252062b4521be3699f0ad17
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-15 18:16:56 +0000

    Aula_84 -> Gerando Thumbnail (GD)

--- Secao_16/Aula_84.php
+++ Secao_16/Aula_84.php
@@ -2,4 +2,14 @@
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
 
+	//header("Content-type: image/jpeg");
+
+	$file = "wallpaper.jpg";
+
+	$new_width = 256;
+	$new_height = 256;
+
+	//var_dump(getimagesize($file));
+	getimagesize($file);
+
 ?>
