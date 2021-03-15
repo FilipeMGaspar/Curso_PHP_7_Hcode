89c04f84badbe974f5d63718885ec5199e3d89c0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-15 17:42:50 +0000

    Aula_82 -> Usando imagem existente (GD)

--- Secao_16/Aula_82.php
+++ Secao_16/Aula_82.php
@@ -7,4 +7,10 @@ require_once "../mostraerros.php";
 	$titleColor = imagecolorallocate($image, 0, 0, 0);
 
 	$gray = imagecolorallocate($image, 100, 100, 100);
+
+	imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
+
+	imagestring($image, 5, 440, 350, "Divanei Aparecida", $titleColor);
+
+	imagestring($image, 3, 440, 370, "concluido em: ".date("d/m/Y"), $titleColor);
 ?>
