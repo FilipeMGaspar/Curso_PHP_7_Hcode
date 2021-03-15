900cf83670330b1c5b533fc678640e642a1d37b0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-15 13:02:22 +0000

    Aula_81 -> Processamento de Imagem (GD)

--- Secao_16/Aula_81.php
+++ Secao_16/Aula_81.php
@@ -5,4 +5,10 @@ require_once "../mostraerros.php";
 	header("Content-Type: image/png");
 
 	$image = imagecreate(256, 256);
+
+	$black = imagecolorallocate($image, 0, 0, 0);
+	$red = imagecolorallocate($image, 255, 0, 0);
+
+	
+
 ?>
