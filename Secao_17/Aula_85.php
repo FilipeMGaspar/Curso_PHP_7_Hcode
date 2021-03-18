b8fbeab48468839e38463135634f1831cdc7f690
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-18 09:05:48 +0000

    Aula_85 -> Boleto PHP

--- Secao_17/Aula_85.php
+++ Secao_17/Aula_85.php
@@ -34,64 +34,64 @@ require_once "../mostraerros.php";
 // ------------------------- DADOS DINÂMICOS DO SEU CLIENTE PARA A GERAÇÃO DO BOLETO (FIXO OU VIA GET) -------------------- //
 // Os valores abaixo podem ser colocados manualmente ou ajustados p/ formulário c/ POST, GET ou de BD (MySql,Postgre,etc)	//
 
-// DADOS DO BOLETO PARA O SEU CLIENTE
-$dias_de_prazo_para_pagamento = 5;
-$taxa_boleto = 2.95;
-$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-$valor_cobrado = "2950,00"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
-$valor_cobrado = str_replace(",", ".",$valor_cobrado);
-$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+	// DADOS DO BOLETO PARA O SEU CLIENTE
+	$dias_de_prazo_para_pagamento = 5;
+	$taxa_boleto = 2.95;
+	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+	$valor_cobrado = "2950,00"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+	$valor_cobrado = str_replace(",", ".",$valor_cobrado);
+	$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
-$dadosboleto["nosso_numero"] = "1803029901";  // Nosso numero - REGRA: Máximo de 14 caracteres!
-$dadosboleto["numero_documento"] = "18.030299.01";	// Num do pedido ou do documento
-$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
-$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
-$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+	$dadosboleto["nosso_numero"] = "1803029901";  // Nosso numero - REGRA: Máximo de 14 caracteres!
+	$dadosboleto["numero_documento"] = "18.030299.01";	// Num do pedido ou do documento
+	$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+	$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+	$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+	$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
 
-// DADOS DO SEU CLIENTE
-$dadosboleto["sacado"] = "Nome do seu Cliente";
-$dadosboleto["endereco1"] = "Endereço do seu Cliente";
-$dadosboleto["endereco2"] = "Cidade - Estado -  CEP: 00000-000";
+	// DADOS DO SEU CLIENTE
+	$dadosboleto["sacado"] = "Nome do seu Cliente";
+	$dadosboleto["endereco1"] = "Endereço do seu Cliente";
+	$dadosboleto["endereco2"] = "Cidade - Estado -  CEP: 00000-000";
 
-// INFORMACOES PARA O CLIENTE
-$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Nonononono";
-$dadosboleto["demonstrativo2"] = "Mensalidade referente a nonon nonooon nononon<br>Taxa bancária - R$ ".number_format($taxa_boleto, 2, ',', '');
-$dadosboleto["demonstrativo3"] = "BoletoPhp - http://www.boletophp.com.br";
-$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
-$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
-$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: xxxx@xxxx.com.br";
-$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
+	// INFORMACOES PARA O CLIENTE
+	$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Nonononono";
+	$dadosboleto["demonstrativo2"] = "Mensalidade referente a nonon nonooon nononon<br>Taxa bancária - R$ ".number_format($taxa_boleto, 2, ',', '');
+	$dadosboleto["demonstrativo3"] = "BoletoPhp - http://www.boletophp.com.br";
+	$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+	$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+	$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: xxxx@xxxx.com.br";
+	$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
 
-// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
-$dadosboleto["quantidade"] = "";
-$dadosboleto["valor_unitario"] = "";
-$dadosboleto["aceite"] = "";		
-$dadosboleto["especie"] = "R$";
-$dadosboleto["especie_doc"] = "DM";
-
-
-// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+	// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+	$dadosboleto["quantidade"] = "";
+	$dadosboleto["valor_unitario"] = "";
+	$dadosboleto["aceite"] = "";		
+	$dadosboleto["especie"] = "R$";
+	$dadosboleto["especie_doc"] = "DM";
 
 
-// DADOS DA SUA CONTA - UNIBANCO
-$dadosboleto["agencia"] = "0123"; // Num da agencia, sem digito
-$dadosboleto["conta"] = "100618"; 	// Num da conta, sem digito
-$dadosboleto["conta_dv"] = "9"; 	// Digito do Num da conta
+	// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
 
-// DADOS PERSONALIZADOS - UNIBANCO
-$dadosboleto["codigo_cliente"] = "2031671"; // Codigo do Cliente
-$dadosboleto["carteira"] = "20";  // Código da Carteira
 
-// SEUS DADOS
-$dadosboleto["identificacao"] = "BoletoPhp - Código Aberto de Sistema de Boletos";
-$dadosboleto["cpf_cnpj"] = "";
-$dadosboleto["endereco"] = "Coloque o endereço da sua empresa aqui";
-$dadosboleto["cidade_uf"] = "Cidade / Estado";
-$dadosboleto["cedente"] = "Coloque a Razão Social da sua empresa aqui";
+	// DADOS DA SUA CONTA - UNIBANCO
+	$dadosboleto["agencia"] = "0123"; // Num da agencia, sem digito
+	$dadosboleto["conta"] = "100618"; 	// Num da conta, sem digito
+	$dadosboleto["conta_dv"] = "9"; 	// Digito do Num da conta
 
-// NÃO ALTERAR!
-include("include/funcoes_unibanco.php"); 
-include("include/layout_unibanco.php");
+	// DADOS PERSONALIZADOS - UNIBANCO
+	$dadosboleto["codigo_cliente"] = "2031671"; // Codigo do Cliente
+	$dadosboleto["carteira"] = "20";  // Código da Carteira
+
+	// SEUS DADOS
+	$dadosboleto["identificacao"] = "BoletoPhp - Código Aberto de Sistema de Boletos";
+	$dadosboleto["cpf_cnpj"] = "";
+	$dadosboleto["endereco"] = "Coloque o endereço da sua empresa aqui";
+	$dadosboleto["cidade_uf"] = "Cidade / Estado";
+	$dadosboleto["cedente"] = "Coloque a Razão Social da sua empresa aqui";
+
+	// NÃO ALTERAR!
+	include("include/funcoes_unibanco.php"); 
+	include("include/layout_unibanco.php");
 ?>
 ?>
