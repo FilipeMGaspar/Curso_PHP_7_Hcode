316076a3937fa5f58ba12667c5fe85be44bf3190
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-19 16:43:01 +0000

    Aula_92 -> SQL Injection

--- Secao_23/Aula_92.php
+++ Secao_23/Aula_92.php
@@ -2,5 +2,9 @@
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
 	
+	$id = (isset($_GET["id"]));
+
 	$conn = mysqli("localhost" , "admin", "123", "db_php7");
+
+	$sql = "SELECT * FROM tb_usuarios WHERE idusuario = $id";
 ?>
