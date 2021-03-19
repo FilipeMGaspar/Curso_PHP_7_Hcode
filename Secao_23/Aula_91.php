c62b400fe42041f23ef1e93345bc248c115f23a2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-19 16:08:48 +0000

    Aula_91 -> Command Injection

--- Secao_23/Aula_91.php
+++ Secao_23/Aula_91.php
@@ -4,8 +4,13 @@ require_once "../mostraerros.php";
 	
 	echo "<pre>";
 
-	$comando = system("ls /var/www/html", $retorno);
+	$comando = system("ls /", $retorno);
 	//$comando = system("dir C:", $retorno);
 
 	echo "</pre>";
 ?>
+
+<form method="post">
+	<input type="text" name="cmd">
+	<button type="submit">Enviar</button>
+</form>
