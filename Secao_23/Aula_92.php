ee0374de170518ed03446158a6e4c25f005440fb
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-19 16:59:11 +0000

    Aula_92 -> SQL Injection

--- Secao_23/Aula_92.php
+++ Secao_23/Aula_92.php
@@ -4,6 +4,10 @@ require_once "../mostraerros.php";
 	
 	$id = (isset($_GET["id"]))?$_GET["id"]:7;
 
+	if(!is_numeric$id){
+		exit("Foste Apanhado!");
+	}
+
 	$conn = mysqli_connect("localhost" , "admin", "123", "db_php7");
 
 	$sql = "SELECT * FROM tb_usuarios WHERE idusuario = $id";
