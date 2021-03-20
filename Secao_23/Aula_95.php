af0c7aebc387dd22c528f1c83e2778725a961157
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-20 10:42:11 +0000

    Aula_95 -> Cross-site_Scripting_(XSS)

--- Secao_23/Aula_95.php
+++ Secao_23/Aula_95.php
@@ -1,6 +1,11 @@
+<form method="post">
+	<input type="text" name="busca">
+	<button type="submit">Enviar</button>
+</form>
+
 <?php
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
 	
-	
+
 ?>
