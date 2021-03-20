20ccc85d6f3a86e40f09c8f041c9f50d0253d1e6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-20 10:54:12 +0000

    Aula_95 -> Cross-site_Scripting_(XSS)

--- Secao_23/Aula_95.php
+++ Secao_23/Aula_95.php
@@ -6,6 +6,8 @@
 <?php
 //Incluir script que permite mostrar os erros
 require_once "../mostraerros.php";
+
+	$_POST["busca"] = '<a href="#"><strong>OLA!</strong></a><script>alert("Estás tramado!")</script>';
 	
 	if(isset($_POST["busca"])){
 		echo $_POST["busca"];
