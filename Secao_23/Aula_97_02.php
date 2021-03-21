32a5b101749bac3daad6826acc2d80333115c655
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-21 14:14:14 +0000

    Aula_97 -> Criptografia em PHP

---
+++ Secao_23/Aula_97_02.php
@@ -0,0 +1,8 @@
+<?php
+//Incluir script que permite mostrar os erros
+require_once "../mostraerros.php";
+
+	define('SECRET_IV', pack('a16', 'senha'));
+	define('SECRET', pack('a16', 'senha'));
+	
+?>
