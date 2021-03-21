f94dab9fb49ce17cb20c314f1cef2e0a3e93b40a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-03-21 15:18:49 +0000

    Aula_98 -> SSL e PHP Session Hijacking

--- Secao_23/Aula_98.php
+++ Secao_23/Aula_98.php
@@ -4,5 +4,12 @@ require_once "../mostraerros.php";
 	
 	session_start();
 
+	//Depois de verificar login e senha reinicie o ID da seção
+	session_destroy();
+	
+	session_start();
+	session_regenerate_id();
+
 	echo session_id();
+
 ?>
